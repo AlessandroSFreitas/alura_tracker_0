432072e86214cd7605c7fce31bccccef7a348886
Alessandro Freitas <alessandro.spiercort@hotmail.com>
2022-11-22 08:19:34 -0300

    implement mutations on alura tracker

--- src/components/Formulario.vue
+++ src/components/Formulario.vue
@@ -1,8 +1,18 @@
 <template>
     <div class="box formulario">
         <div class="columns">
-            <div class="column is-8" role="form" aria-label="Formulário para criação de uma nova tarefa">
+            <div class="column is-5" role="form" aria-label="Formulário para criação de uma nova tarefa">
                 <input type="text" class="input" placeholder="Qual tarefa você deseja iniciar?" v-model="descricao">
+            </div>
+            <div class="column is-3">
+              <div class="select">
+                <select v-model="idProjeto">
+                  <option value="">Selecione o projeto</option>
+                  <option :value="projeto.id" v-for="projeto in projetos" :key="projeto.id">
+                    {{ projeto.nome }}
+                  </option>
+                </select>
+              </div>
             </div>
             <div class="column">
                 <Temporizador @ao-temporizador-finalizado="finalizarTarefa"/>
@@ -12,8 +22,10 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { computed, defineComponent } from 'vue';
+import { useStore } from 'vuex';
 import Temporizador from './Temporizador.vue';
+import { key } from '@/store';
 
 export default defineComponent({
   name: "Formulario",
@@ -22,16 +34,26 @@ export default defineComponent({
     Temporizador
   },
   data () {
-    return { descricao: '' };
+    return {
+      descricao: '',
+      idProjeto: '',
+    };
   },
   methods: {
     // metodo responsavel por armazenar o estado atual do tempo decorrido.
     finalizarTarefa (tempoDecorrido: number) : void {
         this.$emit('aoSalvarTarefa', {
             duracaoEmSegundos: tempoDecorrido,
-            descricao: this.descricao
+            descricao: this.descricao,
+            projeto: this.projetos.find(proj => proj.id == this.idProjeto),
         })
         this.descricao = ''
+    }
+  },
+  setup () {
+    const store = useStore(key)
+    return {
+      projetos: computed(() => store.state.projetos)
     }
   },
 });
